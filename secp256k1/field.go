a722d680fb618776f673e243aeea44de65e7da25
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-09-09 11:09:53 +0200

    Implement some basic secp256k1 field operations.

---
+++ secp256k1/field.go
@@ -0,0 +1,80 @@
+package secp256k1
+
+import (
+	"errors"
+
+	"github.com/cronokirby/saferith"
+)
+
+// p is the modulus for the field used in secp256k1.
+var p *saferith.Modulus
+
+func init() {
+	p, _ = saferith.ModulusFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F")
+}
+
+// Field represents an element in the prime field used by secp256k1.
+//
+// This field is used later to implement point operations on the curve.
+type Field struct {
+	nat saferith.Nat
+}
+
+// NewField creates a new field element, with its value set to 0.
+func NewField() *Field {
+	var nat saferith.Nat
+	// This will conveniently set the announced size, and mark this number as reduced modulo p.
+	nat.Mod(&nat, p)
+	return &Field{nat: nat}
+}
+
+// Add calculates z <- z + a, returning z.
+func (z *Field) Add(a *Field) *Field {
+	z.nat.ModAdd(&z.nat, &a.nat, p)
+	return z
+}
+
+// Sub calculates z <- z - a, returning z.
+func (z *Field) Sub(a *Field) *Field {
+	z.nat.ModSub(&z.nat, &a.nat, p)
+	return z
+}
+
+// Mul calculates z <- z * a, returning z.
+func (z *Field) Mul(a *Field) *Field {
+	z.nat.ModMul(&z.nat, &a.nat, p)
+	return z
+}
+
+// Invert calculates z <- z^-1, returning z.
+func (z *Field) Invert() *Field {
+	z.nat.ModInverse(&z.nat, p)
+	return z
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface.
+//
+// This encodes the field element as big endian bytes. The result will always occupy
+// 32 bytes of space.
+func (z *Field) MarshalBinary() ([]byte, error) {
+	// We do this to make sure that the announced size is padded to the size of p,
+	// in case z was created wihout calling NewField, or something like that.
+	// Since z will always be reduced modulo p, this doesn't actually cost anything.
+	z.nat.Mod(&z.nat, p)
+	return z.nat.Bytes(), nil
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface.
+//
+// This expects exactly 32 Big Endian bytes, and will also return an error if the
+// resulting value is >= the field modulus.
+func (z *Field) UnmarshalBinary(data []byte) error {
+	if len(data) != p.BitLen()/8 {
+		return errors.New("secp256k1.Field.UnmarshalBinary: invalid data length")
+	}
+	z.nat.SetBytes(data)
+	if _, _, lt := z.nat.CmpMod(p); lt != 1 {
+		return errors.New("secp256k1.Field.UnmarshalBinary: value is greater than field prime")
+	}
+	return nil
+}
