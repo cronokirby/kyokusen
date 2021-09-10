a052e0b84ef32c920f5b9d055f799a6f8f96e2b6
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-09-10 13:08:02 +0200

    Add stub methods for secp256k1 points

--- secp256k1/point.go
+++ secp256k1/point.go
@@ -1,5 +1,7 @@
 package secp256k1
 
+import "github.com/cronokirby/kyokusen"
+
 // Point represents a point on the secp256k1 curve.
 type Point struct {
 	// Internally, we represent this as a projective point (X : Y : Z).
@@ -8,4 +10,49 @@ type Point struct {
 	x *Field
 	y *Field
 	z *Field
+}
+
+func (*Point) MarshalBinary() ([]byte, error) {
+	// TODO: Implement
+	return nil, nil
+}
+
+func (*Point) UnmarshalBinary(data []byte) error {
+	// TODO: Implement
+	return nil
+}
+
+func (*Point) Curve() kyokusen.Curve {
+	// TODO: Implement
+	return nil
+}
+
+func (*Point) Add(kyokusen.Point) kyokusen.Point {
+	// TODO: Implement
+	return nil
+}
+
+func (*Point) Sub(kyokusen.Point) kyokusen.Point {
+	// TODO: Implement
+	return nil
+}
+
+func (*Point) Negate() kyokusen.Point {
+	// TODO: Implement
+	return nil
+}
+
+func (*Point) Equal(kyokusen.Point) bool {
+	// TODO: Implement
+	return false
+}
+
+func (*Point) IsIdentity() bool {
+	// TODO: Implement
+	return false
+}
+
+func (*Point) XScalar() kyokusen.Scalar {
+	// TODO: Implement
+	return nil
 }
